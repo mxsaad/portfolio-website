9235e696e863ecbede9710e863cbbc96e2dfed27
Muhammad Saad <msaad3@proton.me>
2022-07-18 12:15:26 -0400

    Add responsive header

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { FaTimes } from 'react-icons/fa';
+
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+  
+  return (
+    <header className='fixed w-full h-20 bg-mk-black flex justify-between items-center px-2 rounded-b-2xl'>
+        {/* Name */}
+        <button className={!menuOpen ? 'font-nautigal text-mk-pink text-5xl px-2 hover:text-mk-yellow hover:scale-105 duration-150 z-10'
+        : 'font-nautigal text-mk-yellow text-5xl px-2 hover:text-mk-pink hover:scale-105 duration-150  z-10'}>
+          MS
+        </button>
+        
+        {/* Desktop Menu */}
+        <menu className='hidden md:flex text-mk-pink gap-5 px-4 text-2xl'>
+          <li><button className='hover:text-mk-yellow hover:scale-105 duration-150'>About</button></li>
+          <li><button className='hover:text-mk-yellow hover:scale-105 duration-150'>Skills</button></li>
+          <li><button className='hover:text-mk-yellow hover:scale-105 duration-150'>Work</button></li>
+          <li><button className='hover:text-mk-yellow hover:scale-105 duration-150'>Connect</button></li>
+        </menu>
+
+        {/* Hamburger (Mobile Only) */}
+        <button onClick={() => toggleMenu()} className='md:hidden z-10 px-2 text-4xl hover:scale-105 duration-150'>
+          {!menuOpen ? <BiMenuAltRight className='text-mk-pink hover:text-mk-yellow duration-150'/> 
+          : <FaTimes className='text-mk-yellow hover:text-mk-pink duration-150'/>}
+        </button>
+
+        {/* Mobile Menu */}
+        <menu className={!menuOpen ? 'hidden' : 'flex flex-col justify-center items-center gap-10 absolute top-0 left-0 w-full h-screen bg-mk-black text-5xl text-mk-yellow'}>
+          <li><button className='hover:text-mk-pink hover:scale-105 duration-150'>About</button></li>
+          <li><button className='hover:text-mk-pink hover:scale-105 duration-150'>Skills</button></li>
+          <li><button className='hover:text-mk-pink hover:scale-105 duration-150'>Work</button></li>
+          <li><button className='hover:text-mk-pink hover:scale-105 duration-150'>Connect</button></li>
+        </menu>
+    </header>
+  );
+}
+
+export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mk-black': '#211F22',
+        'mk-gray': '#2D282D',
+        'mk-pink': '#F92672',
+        'mk-blue': '#66D9EF',
+        'mk-green': '#A6E22E',
+        'mk-orange': '#FD971F',
+        'mk-yellow': '#E5B567',
+      },
+      fontFamily: {
+        'lato': 'Lato, sans-serif;',
+        'nautigal': "'The Nautigal', cursive;",
+      }
+    }
   },
   plugins: [],
 }
