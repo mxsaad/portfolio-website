481e7b4b79f32eed300cccbeee762f3305788d97
Muhammad Saad <msaad3@proton.me>
2023-05-06 22:36:33 -0400

    Enable Firestore for Skills
    *No longer need data/img files

--- src/components/ShowcaseItem.js
+++ src/components/ShowcaseItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ShowcaseItem = (props) => {
   return (
     <div className='flex flex-shrink-0 flex-col justify-center items-center w-32 h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 gap-4 py-6 bg-mk-gray border-2 border-mk-pink rounded-2xl'>
-        <img src={props.icon} alt="Icon for showcase item." className='w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 duration-150'/>
+        <img src={props.icon} alt={`${props.name} icon.`} className='w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 duration-150'/>
         <p className='text-mk-yellow text-lg md:text-xl lg:text-2xl duration-150'>{props.name}</p>
     </div>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ShowcaseItem from './ShowcaseItem';
-import languages from '../data/skills_languages.json';
-import technologies from '../data/skills_technologies.json';
+import db from '../firebase';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+
 
 const Skills = () => {
+  const [languages, setLanguages] = useState([]);
+  const [technologies, setTechnologies] = useState([]);
+
+  const langRef = collection(db, 'language-skills');
+  const techRef = collection(db, 'technology-skills');
+
+  const getSkills = (ref, setSkills) => {
+    onSnapshot(query(ref, orderBy('order')), (querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setSkills(items);
+    });
+  }
+
+  useEffect(() => {
+    getSkills(langRef, (items) => setLanguages(items));
+    getSkills(techRef, (items) => setTechnologies(items));
+  })
+
   return (
     <section name='skills' className='w-full h-screen flex flex-col justify-center gap-8'>
       <div className='flex flex-col justify-center gap-8'>
@@ -18,14 +40,14 @@ const Skills = () => {
         {/* Languages */}
         <div className='w-full flex py-4 px-10 md:px-14 lg:px-20 gap-4 overflow-x-auto'>
           { languages.map((lang, i) => {
-            return <ShowcaseItem key={i} name={lang.name} icon={require(`../img/icon/${lang.img}`)} />;
+            return <ShowcaseItem key={i} name={lang.name} icon={lang.img}/>;
           })}
         </div>
 
         {/* Technologies */}
         <div className='w-full flex py-4 px-10 md:px-14 lg:px-20 gap-4 overflow-x-auto'>
           { technologies.map((tech, i) => {
-            return <ShowcaseItem key={i} name={tech.name} icon={require(`../img/icon/${tech.img}`)} />;
+            return <ShowcaseItem key={i} name={tech.name} icon={tech.img}/>;
           })}
         </div>
       </div>
